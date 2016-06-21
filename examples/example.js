f8c4411d409454cd26f23d738922c69360ee4d09
Shane Thacker <shane@steadymade.com>
2016-06-21 14:25:55 -0500

    change from tabs to spaces

--- examples/example.js
+++ examples/example.js
@@ -6,40 +6,40 @@ var Beard = require('beard');
 
 
 http.createServer(function (request, response) {
-	response.writeHead(200, {'Content-Type': 'text/html'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
 
-	fs.readFile(__dirname + '/example.html', 'UTF-8', function(err, template){
-		if (err) throw err;
-		var view = {
-			page: {
-				title: 'Grow a Beard'
-			},
-			index: {
-				'yo': {
-					name: { first: 'Charles', last: 'Spurgeon' }
-				},
-				'mama': {
-					name: { first: 'John', last: 'Calvin' }
-				}
-			},
-			collection: [
-				{
-					name: { first: 'Jack', last: 'Black' }
-				},
-				{
-					name: { first: 'Chuck', last: 'Norris' }
-				}
-			],
-			subhead: 'Beard Examples',
-			nacho: 'foo',
-			cats: ['jack','black','attack'],
-			makeUpperCase: function(str){
-				return str.toUpperCase();
-			}
-		};
-		var html = Beard.render(template, view);
-		response.end(html);
-	});
+    fs.readFile(__dirname + '/example.html', 'UTF-8', function(err, template){
+        if (err) throw err;
+        var view = {
+            page: {
+                title: 'Grow a Beard'
+            },
+            index: {
+                'yo': {
+                    name: { first: 'Charles', last: 'Spurgeon' }
+                },
+                'mama': {
+                    name: { first: 'John', last: 'Calvin' }
+                }
+            },
+            collection: [
+                {
+                    name: { first: 'Jack', last: 'Black' }
+                },
+                {
+                    name: { first: 'Chuck', last: 'Norris' }
+                }
+            ],
+            subhead: 'Beard Examples',
+            nacho: 'foo',
+            cats: ['jack','black','attack'],
+            makeUpperCase: function(str){
+                return str.toUpperCase();
+            }
+        };
+        var html = Beard.render(template, view);
+        response.end(html);
+    });
 
 }).listen(5555);
 
