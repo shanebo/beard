a2704591692c4ee0ea079eb0d71a804f82637801
Shane Thacker <shane@steadymade.com>
2018-01-03 16:40:10 -0600

    Next pass on making beard better

--- beard.js
+++ beard.js
@@ -1,188 +1,122 @@
-(function(){
+const Beard = function() {
 
-var Beard = function(cache){
-    this._cache = cache;
 }
-
-var iterator = 0;
-var ignores = {};
-var ignoreId = 0;
-
-var exps = {
-    extend: (/{extend\s(.*?)}/),
-    include: (/{include\s(.*?)}/g),
-    block: (/{block\s+(.[^}]*)}([^]*?){endblock}/g),
-    ignore: (/{ignore}([^]*?){endignore}/g),
-    ignoreTemp: (/_ignore_([^]*?)_endignore_/g),
-    statement: (/\{\s*([^}]+?)\s*\}/g),
-    operators: (/\s+(and|or|eq|neq|is|isnt|not)\s+/g),
-    if: (/^if\s+([^]*)$/),
-    elseIf: (/^else\s+if\s+([^]*)$/),
-    else: (/^else$/),
-    for: (/^for\s+([$A-Za-z_][0-9A-Za-z_]*)(?:\s*,\s*([$A-Za-z_][0-9A-Za-z_]*))?\s+in\s+(.*)$/),
-    each: (/^each\s+([$A-Za-z_][0-9A-Za-z_]*)(?:\s*,\s*([$A-Za-z_][0-9A-Za-z_]*))?\s+in\s(.*)$/),
-    end: (/^end$/)
-};
-
-var operators = {
-    and: ' && ',
-    or: ' || ',
-    eq: ' === ',
-    neq: ' !== ',
-    not: ' !',
-    isnt: ' != ',
-    is: ' == '
-};
-
-var parse = {
-
-    ignore: function(_, contents) {
-        ignoreId += 1;
-        ignores[ignoreId] = contents;
-        return '_ignore_' + ignoreId + '_endignore_';
-    },
-
-    ignoreTemp: function(_, id) {
-        return ignores[id];
-    },
-
-    operators: function(_, op) {
-        return operators[op];
-    },
-
-    if: function(_, statement) {
-        return 'if (' + statement + ') {';
-    },
-
-    elseIf: function(_, statement) {
-        return '} else if (' + statement +') {';
-    },
-
-    else: function() {
-        return '} else {';
-    },
-
-    for: function(_, key, value, object) {
-        if (!value) key = (value = key, 'iterator' + iterator++);
-        return 'for (var ' + key + ' in ' + object + '){' + 'var ' + value + ' = ' + object + '[' + key +'];';
-    },
-
-    each: function(_, iter, value, array) {
-        if (!value) iter = (value = iter, 'iterator' + iterator++);
-        var length = 'length' + iterator++;
-        return 'for (var ' + iter + ' = 0, ' + length + ' = ' + array + '.length; ' + iter + ' < ' + length + '; ' + iter + '++) {' + 'var ' + value + ' = ' + array + '[' + (iter) + '];';
-    },
-
-    end: function() {
-        return '}';
-    }
-};
 
 Beard.prototype = {
-
-    parser: function(match, inner) {
-        var prev = inner;
-        inner = inner
-            .replace(exps.operators, parse.operators)
-            .replace(exps.end, parse.end)
-            .replace(exps.else, parse.else)
-            .replace(exps.elseIf, parse.elseIf)
-            .replace(exps.if, parse.if)
-            .replace(exps.each, parse.each)
-            .replace(exps.for, parse.for);
-
-        return '";' + (inner === prev ? ' _buffer += ' : '') + inner.replace(/\t|\n|\r/, '') + '; _buffer += "';
-    },
-
-    parseExtend: function(template, data) {
-        var matches;
-        template = template.replace(exps.extend, function(){
-            matches = ([]).slice.call(arguments, 0);
-            return '';
-        });
-
-        if (matches && matches.length) {
-            var path = matches[1];
-            var view = this._cache[path];
-            view = view.replace(exps.ignore, parse.ignore);
-            view += "{block view}" + this.preRender(template, data) + "{endblock}";
-            return view;
-        } else {
-            return template;
-        }
-    },
-
-    parseInclude: function(template, data) {
-        return template.replace(exps.include, function(_, path){
-            return this.preRender(this._cache[path], data);
-        }.bind(this));
-    },
-
-    parseBlock: function(template, data) {
-        var matches = [];
-        template = template.replace(exps.block, function(){
-            var arr = ([]).slice.call(arguments, 0);
-            matches.push(arr);
-            return '';
-        });
-
-        matches.forEach(function(set){
-            // set[1] is the var name;
-            // set[2] is the var value;
-            data[set[1]] = this.compile(set[2])(data);
-        }.bind(this));
-
-        return this.compile(template)(data);
-    },
-
-    preRender: function(template, data){
-        template = template.replace(exps.ignore, parse.ignore);
-        template = this.parseExtend(template, data);
-        template = this.parseInclude(template, data);
-        template = this.parseBlock(template, data);
-        return this.compile(template)(data);
-    },
-
-    render: function(template, data){
-        template = this.preRender(template, data);
-        template = template.replace(exps.ignoreTemp, parse.ignoreTemp);
-        ignoreId = 0;
-        ignores = {};
-        return template;
-    },
-
-    compile: function(str) {
-        str = str
-            .replace(new RegExp('\\\\', 'g'), '\\\\').replace(/"/g, '\\"')
-            .replace(exps.statement, this.parser)
-            .replace(/_buffer_\s\+=\s"";/g, '')
-            .replace(/(\{|\});/g, '$1')
-            .replace(/\n/g, '\\n')
-            .replace(/\t/g, '\\t')
-            .replace(/\r/g, '\\r');
-
-        var fn = (
-            'var _buffer = ""; \
-             for (var prop in _data_) { \
-                if (_data_.hasOwnProperty(prop)) this[prop] = _data_[prop]; \
-             } \
-             _buffer += "' + str + '"; \
-             return _buffer;'
-        );
-
-        try {
-            return new Function('_data_', fn);
-        } catch (e) {
-            throw new Error('Cant compile template:' + fn);
-        }
-    }
+  render: (template, data) => compile(template)(data)
 };
 
+// should we reset the iterator to zero every time render is called so it doesnt build up too large?
+let iterator = 0;
 
-if (typeof module !== 'undefined') {
-    module.exports = Beard;
-} else {
-    window.Beard = Beard;
+const exps = {
+  include:    (/^include\s(.*?)$/g),
+  block:      (/{block\s+(.[^}]*)}([^]*?){endblock}/g),
+  statement:  (/\{\s*([^}]+?)\s*\}/g),
+  if:         (/^if\s+([^]*)$/),
+  elseIf:     (/^else\s+if\s+([^]*)$/),
+  else:       (/^else$/),
+  for:        (/^for\s+([$A-Za-z_][0-9A-Za-z_]*)(?:\s*,\s*([$A-Za-z_][0-9A-Za-z_]*))?\s+in\s+(.*)$/),
+  each:       (/^each\s+([$A-Za-z_][0-9A-Za-z_]*)(?:\s*,\s*([$A-Za-z_][0-9A-Za-z_]*))?\s+in\s(.*)$/),
+  end:        (/^end$/)
+};
+
+const parse = {
+  include:    (_, path) => `_buffer += compile("${_cache[path]}")(_data_)`, // eventually we'll need to add support to pass in a _cache object
+  block:      (_, varname, content) => `{:var ${varname} = compile("${content}")(_data_)}`,
+  if:         (_, statement) => `if (${statement}) {`,
+  elseIf:     (_, statement) => `} else if (${statement}) {`,
+  else:       () => '} else {',
+  end:        () => '}',
+
+  for: (_, key, value, object) => {
+    if (!value) key = (value = key, 'iterator' + iterator++);
+    return `for (var ${key} in ${object}){ var ${value} = ${object}[${key}];`;
+  },
+
+  each: (_, iter, value, array) => {
+    if (!value) iter = (value = iter, 'iterator' + iterator++);
+    var length = 'length' + iterator++;
+    return `for (var ${iter} = 0, ${length} = ${array}.length; ${iter} < ${length}; ${iter}++) { var ${value} = ${array}[${(iter)}];`;
+  }
+};
+
+function parser(match, inner) {
+  const prev = inner;
+  inner = inner
+    .replace(exps.include, parse.include)
+    .replace(exps.end, parse.end)
+    .replace(exps.else, parse.else)
+    .replace(exps.elseIf, parse.elseIf)
+    .replace(exps.if, parse.if)
+    .replace(exps.each, parse.each)
+    .replace(exps.for, parse.for);
+
+  return `"; ${(inner === prev && !/^:/.test(inner) ? ' _buffer += ' : '')} ${inner.replace(/\t|\n|\r|:/, '')}; _buffer += "`;
 }
 
-})();
+function compile(str) {
+  str = str
+    .replace(new RegExp('\\\\', 'g'), '\\\\').replace(/"/g, '\\"')
+    .replace(exps.block, parse.block)
+    .replace(exps.statement, parser)
+    .replace(/_buffer_\s\+=\s"";/g, '')
+    .replace(/(\{|\});/g, '$1')
+    .replace(/\n/g, '\\n')
+    .replace(/\t/g, '\\t')
+    .replace(/\r/g, '\\r');
+
+  const fn = `
+    function _compiledTemplate_(_data_){
+      var _buffer = "";
+      for (var prop in _data_) {
+        if (_data_.hasOwnProperty(prop)) {
+          this[prop] = _data_[prop];
+        }
+      }
+      _buffer += "${str}";
+      return _buffer;
+    }
+  `;
+
+  console.log('\n');
+  console.log(fn);
+
+  try {
+    eval(fn);
+    return _compiledTemplate_;
+  } catch (e) {
+    throw new Error(`Compilation error: ${fn}`);
+  }
+}
+
+module.exports = Beard;
+
+
+
+
+// This is a template cache so you can test partials, blocks, layouts and such
+
+let _cache = {
+  'partials/joe': "<h1>Hello, my name is {name} {if name == 'Joe'}{include partials/lastname}{end}</h1>",
+  'partials/complex-test': '{block doitlive}{include partials/joe}{endblock} -- outside block -- {doitlive}',
+  'foobar': '{block nice}inside block{endblock} -- outside block -- {nice}',
+  'partials/lastname': 'Osburn and I\'m {include partials/winning}',
+  'partials/js': "yo {true ? 'true' : 'not true'}",
+  'partials/winning': 'winning with Shane.'
+};
+
+// These are direct template tests so you don't have to do it in beard files for now
+
+console.log(compile("{include partials/js}", {}));
+
+console.log(compile("{include partials/joe}")({
+  name: 'Joe'
+}));
+
+console.log(compile("{include foobar}")({}));
+
+// this one isn't working
+// console.log(compile("{include partials/complex-test}")({
+//   name: 'Shane'
+// }));
--- examples/example.js
+++ examples/example.js
@@ -1,46 +1,54 @@
+const http = require('http');
+const fs = require('fs');
+const Beard = require('../beard');
+const engine = new Beard();
+const template = fs.readFileSync(__dirname + '/example.beard', 'utf8');
 
+const data = {
+  page: {
+    title: 'Grow a Beard'
+  },
+  index: {
+    yo: {
+      name: {
+        first: 'Charles',
+        last: 'Spurgeon'
+      }
+    },
+    mama: {
+      name: {
+        first: 'John',
+        last: 'Calvin'
+      }
+    }
+  },
+  names: [
+    {
+      name: {
+        first: 'Jack',
+        last: 'Black'
+      }
+    },
+    {
+      name: {
+        first: 'Chuck',
+        last: 'Norris'
+      }
+    }
+  ],
+  subhead: 'Beard Examples',
+  nacho: 'foo',
+  cats: ['jack', 'black', 'attack'],
+  makeUpperCase: str => str.toUpperCase()
+};
 
-var http = require('http');
-var fs = require('fs');
-var Beard = require('beard');
-var engine = new Beard();
+const html = engine.render(template, data);
+console.log('\n');
+console.log(html);
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-
-    fs.readFile(__dirname + '/example.beard', 'UTF-8', function(err, template){
-        if (err) throw err;
-        var view = {
-            page: {
-                title: 'Grow a Beard'
-            },
-            index: {
-                'yo': {
-                    name: { first: 'Charles', last: 'Spurgeon' }
-                },
-                'mama': {
-                    name: { first: 'John', last: 'Calvin' }
-                }
-            },
-            collection: [
-                {
-                    name: { first: 'Jack', last: 'Black' }
-                },
-                {
-                    name: { first: 'Chuck', last: 'Norris' }
-                }
-            ],
-            subhead: 'Beard Examples',
-            nacho: 'foo',
-            cats: ['jack','black','attack'],
-            makeUpperCase: function(str){
-                return str.toUpperCase();
-            }
-        };
-        var html = engine.render(template, view);
-        response.end(html);
-    });
-
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(html);
 }).listen(5555);
 
 console.log('Server running at http://127.0.0.1:5555/');
