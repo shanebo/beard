eb34d1ad030d1fb95db49a0c3790d18603497a79
Joe Osburn <joe@jnodev.com>
2019-05-20 15:07:03 -0500

    Bundling tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const normalize = require('path').normalize;
+const fs = require('fs');
 
 const beard = require('../beard');
 
@@ -734,5 +735,75 @@ describe('Custom Tags', function() {
     });
     expect(engine.render('/templates/view').replace(/\s+/g, ' ')).
       to.equal('begin the nav top <h1>hello world</h1> component end');
+  });
+});
+
+describe('Bundling', function() {
+  let engine;
+
+  before(() => engine = beard({ root: __dirname }));
+
+  it('extracts style blocks into scss asset files and imports them in the entry file', function() {
+    expect(engine.render('bundle/simple').replace(/\s+/g, ' ')).to.not.include('body { color: blue; }');
+    const bundledCSS = fs.readFileSync(`${__dirname}/../.beard/simple.ed10418f.scss`, 'utf8').trim();
+    expect(bundledCSS.replace(/\s+/g, ' ')).to.equal('body { color: blue; }');
+    const entryCSS = fs.readFileSync(`${__dirname}/../.beard/entry.css`, 'utf8').trim();
+    expect(entryCSS).to.include("@import './simple.ed10418f.scss';");
+  });
+
+  it('extracts frontend script blocks into js asset files and imports them in the entry file', function() {
+    expect(engine.render('bundle/simple')).to.not.include("document.getElementById('demo').innerHTML = 'hello';");
+    const bundledJS = fs.readFileSync(`${__dirname}/../.beard/simple.6b756e34.js`, 'utf8').trim();
+    expect(bundledJS).to.equal("document.getElementById('demo').innerHTML = 'hello';");
+    const entryCSS = fs.readFileSync(`${__dirname}/../.beard/entry.js`, 'utf8').trim();
+    expect(entryCSS).to.include("import './simple.6b756e34.js';");
+  });
+
+  it('extracts the ssjs script block', function() {
+    engine.render('bundle/simple');
+    const contents = fs.readFileSync(`${__dirname}/../.beard/simple.f710ee38.ssjs.js`, 'utf8').trim();
+    expect(contents.trim().replace(/\s+/g, ' ')).to.equal("console.log('runnimg');");
+  });
+
+  describe('when script or style block has bundle attribute', function() {
+    it('creates a custom bundle entry file', function() {
+      engine.render('bundle/named-bundle');
+      const entryCSS = fs.readFileSync(`${__dirname}/../.beard/alert.css`, 'utf8').trim();
+      expect(entryCSS).to.equal("@import './named-bundle.e6035d8f.scss';");
+      const entryJS = fs.readFileSync(`${__dirname}/../.beard/alert.js`, 'utf8').trim();
+      expect(entryJS).to.equal("import './named-bundle.03e83341.js';");
+    });
+  });
+
+  describe('when style block has lang attribute', function() {
+    it('sets file extension on extracted block file', function() {
+      engine.render('bundle/lang');
+      const bundledCSS = fs.readFileSync(`${__dirname}/../.beard/lang.28d75d5c.less`, 'utf8').trim();
+      expect(bundledCSS.replace(/\s+/g, ' ')).to.equal(`@color: blue; body { color: @color; }`);
+    });
+  });
+
+  describe('when style block is scoped', function() {
+    it('sets custom css class names on css styles and on html elements', function() {
+      expect(engine.render('bundle/scoped')).to.equal('<body><span class="beard-708076192">test</span></body>');
+      const contents = fs.readFileSync(`${__dirname}/../.beard/scoped.9d1d8a1d.scss`, 'utf8').trim();
+      expect(contents.trim().replace(/\s+/g, ' ')).to.equal('.beard-708076192 { color: green; }');
+    });
+
+    it('does not set custom css class names on nested css styles', function() {
+      expect(engine.render('bundle/scoped-nested')).to
+        .equal('<body> <span class="beard-1869046720">test</span> <h1 class="beard-3811121190">These tacos are <em>amazin</em>!</h1> </body>');
+      const contents = fs.readFileSync(`${__dirname}/../.beard/scoped-nested.347c84c8.scss`, 'utf8').trim();
+      expect(contents.trim().replace(/\s+/g, ' ')).to
+        .equal('.beard-1869046720 { color: green; } .beard-3811121190 { color: blue; em { font-style: italic; } }');
+    });
+
+    it('sets custom css class names on nested styles in media elements', function() {
+      expect(engine.render('bundle/scoped-media-elements')).to
+        .equal('<body class="beard-3535131573"> <span class="beard-660489129">test</span> </body>');
+      const contents = fs.readFileSync(`${__dirname}/../.beard/scoped-media-elements.170ebd31.scss`, 'utf8').trim();
+      expect(contents.trim().replace(/\s+/g, ' ')).to
+        .equal('@media screen {.beard-3535131573 { color: green; } .beard-660489129 { color: green; }}');
+    });
   });
 });
