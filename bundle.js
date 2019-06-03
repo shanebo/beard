f99b78f174b5ad24c01b45397b8efa39c32d8eea
Shane Thacker <shane@steadymade.com>
2019-06-03 16:44:25 -0500

    Whitespace

--- bundle.js
+++ bundle.js
@@ -187,9 +187,6 @@ function addScopedCSS(body, styleName, newStyleName) {
 function scopeStyles(path, content, originalBody) {
   let body = originalBody;
 
-  console.log({path});
-
-
   const styles = replaceStyleNames(content, (styleName, style) => {
     const newStyleName = `.beard-${hash(path.replace(root, '') + style)}`;
     body = addScopedCSS(body, styleName, newStyleName);
