2bd78eb71047f72b38d222b00923924400234a59
Shane Thacker <shane@steadymade.com>
2019-06-21 17:51:58 -0500

    Some test cleanup and making dynamic hash magic AMAZIN test code first pass

--- test/bundling.js
+++ test/bundling.js
@@ -1,7 +1,7 @@
 const beard = require('../lib/index');
 const fs = require('fs');
 const chai = require('chai');
-const expect = chai.expect;
+const { assert, expect } = chai;
 chai.use(require('chai-string'));
 
 const read = (file) => fs.readFileSync(`${__dirname}/../.beard/${file}`, 'utf8').trim();
@@ -12,18 +12,27 @@ const contents = (path, ext) => {
 }
 
 const engine = beard({ root: __dirname });
+const hashRegex = (str) => new RegExp(str.replace(/\$b/gm, '(b\-[0-9a-fA-F]{6})'));
+
+// const hashRegex = (actual, expected) => {
+//   const hashedExpected = expected.replace(/\$b/gm, '(b\-[0-9a-fA-F]{8})');
+//   assert(new RegExp(hashedExpected).test(actual), `Expected ${actual} to be ${expected}`);
+// }
+// chai.Assertion.addChainableMethod('matchHashedHTML', hashRegex, chainingBehavior);
+// expect(fooStr).to.be.foo('bar');
+// expect(fooStr).to.be.foo.equal('foo');
 
 describe('Bundling', function() {
   it('extracts style blocks into scss asset files and imports them in the entry file', function() {
     expect(engine.render('templates/simple')).to.not.include('body { color: blue; }');
     expect(contents('simple', 'scss')).to.equalIgnoreSpaces('body { color: blue; }');
-    expect(read('entry.css')).to.include("@import './simple.ed10418f.scss';");
+    expect(read('entry.css')).to.include("@import './simple.");
   });
 
   it('extracts frontend script blocks into js asset files and imports them in the entry file', function() {
     expect(engine.render('templates/simple')).to.not.include("document.getElementById('demo').innerHTML = 'hello';");
     expect(contents('simple', 'js')).to.equalIgnoreSpaces("document.getElementById('demo').innerHTML = 'hello';");
-    expect(read('entry.js')).to.include("import './simple.6b756e34.js';");
+    expect(read('entry.js')).to.include("import './simple.");
   });
 
   it('extracts the ssjs script block', function() {
@@ -32,8 +41,8 @@ describe('Bundling', function() {
 
   describe('when script or style block has bundle attribute', function() {
     it('creates a custom bundle entry file', function() {
-      expect(read('alert.css')).to.equalIgnoreSpaces("@import './named-bundle.e6035d8f.scss';");
-      expect(read('alert.js')).to.equalIgnoreSpaces("import './named-bundle.03e83341.js';");
+      expect(read('alert.css')).to.includes("@import './named-bundle.");
+      expect(read('alert.js')).to.includes("import './named-bundle.");
     });
   });
 
@@ -45,33 +54,33 @@ describe('Bundling', function() {
 
   describe('when style block is scoped', function() {
     it('sets custom css class names on css styles and on html elements', function() {
-      expect(engine.render('templates/scoped')).to.equalIgnoreSpaces('<body><span class="b-bd1f4a0b">test</span></body>');
-      expect(contents('scoped', 'scss')).to.equalIgnoreSpaces('span.b-bd1f4a0b { color: green; }');
+      expect(engine.render('templates/scoped')).to.match(hashRegex('<body><span class="$b">test</span></body>'));
+      expect(contents('scoped', 'scss')).to.match(hashRegex('span.$b { color: green; }'));
     });
 
     it('does not set custom css class names on nested css styles', function() {
-      expect(engine.render('templates/scoped-nested')).to.equalIgnoreSpaces('<body> <span class="b-b10420e7">test</span> <h1 class="b-e210a428">These tacos are <em>amazin</em>!</h1> </body>');
-      expect(contents('scoped-nested', 'scss')).to.equalIgnoreSpaces('span.b-b10420e7 { color: green; } h1.b-e210a428 { color: blue; em { font-style: italic; } }');
+      expect(engine.render('templates/scoped-nested')).to.match(hashRegex('<body> <span class="$b">test</span> <h1 class="$b">These tacos are <em>amazin</em>!</h1> </body>'));
+      expect(contents('scoped-nested', 'scss')).to.match(hashRegex('span.$b { color: green; } h1.$b { color: blue; em { font-style: italic; } }'));
     });
 
     it('sets custom css class names on nested styles in media elements', function() {
-      expect(engine.render('templates/scoped-media-queries')).to.equalIgnoreSpaces('<body class="b-5916699f"> <span class="b-7f731c23">test</span> </body>');
-      expect(contents('scoped-media-queries', 'scss')).to.equalIgnoreSpaces('@media screen {body.b-5916699f { color: green; } span.b-7f731c23 { color: green; }}');
+      expect(engine.render('templates/scoped-media-queries')).to.match(hashRegex('<body class="$b"> <span class="$b">test</span> </body>'));
+      expect(contents('scoped-media-queries', 'scss')).to.match(hashRegex('@media screen {body.$b { color: green; } span.$b { color: green; }}'));
     });
 
     it('sets custom css class names before deep selector', function() {
-      expect(engine.render('templates/scoped-deep')).to.equalIgnoreSpaces('<h1 class="b-5343f3be"><em>hello</em></h1>');
-      expect(contents('scoped-deep', 'scss')).to.equalIgnoreSpaces('h1.b-5343f3be em { color: red; }');
+      expect(engine.render('templates/scoped-deep')).to.match(hashRegex('<h1 class="$b"><em>hello</em></h1>'));
+      expect(contents('scoped-deep', 'scss')).to.match(hashRegex('h1.$b em { color: red; }'));
     });
 
     it('sets custom css class names on chained selectors', function() {
-      expect(engine.render('templates/scoped-chaining')).to.equalIgnoreSpaces('<h1 class="b-c60ed2bf"><em class="b-c60ed2bf">hello h1</em></h1> <h4 class="b-c60ed2bf"><em class="b-c60ed2bf">hello h4</em></h4> <div class="b-c60ed2bf"><em class="b-c60ed2bf">hello div</em></div>');
-      expect(contents('scoped-chaining', 'scss')).to.equalIgnoreSpaces('h1.b-c60ed2bf em.b-c60ed2bf, h4.b-c60ed2bf em.b-c60ed2bf:first-of-type, div.b-c60ed2bf em.b-c60ed2bf { color: green; }');
+      expect(engine.render('templates/scoped-chaining')).to.match(hashRegex('<h1 class="$b"><em class="$b">hello h1</em></h1> <h4 class="$b"><em class="$b">hello h4</em></h4> <div class="$b"><em class="$b">hello div</em></div>'));
+      expect(contents('scoped-chaining', 'scss')).to.match(hashRegex('h1.$b em.$b, h4.$b em.$b:first-of-type, div.$b em.$b { color: green; }'));
     });
 
     it('sets custom css class names selectors with pseudo elements', function() {
-      expect(engine.render('templates/scoped-pseudo-elements')).to.equalIgnoreSpaces('<h1 class="b-025a0994"><em class="b-025a0994">hello h1</em></h1> <h4 class="b-025a0994"><em class="b-025a0994">hello h4</em></h4>');
-      expect(contents('scoped-pseudo-elements', 'scss')).to.equalIgnoreSpaces('h1.b-025a0994 em.b-025a0994, h4.b-025a0994 em.b-025a0994::before { color: green; }');
+      expect(engine.render('templates/scoped-pseudo-elements')).to.match(hashRegex('<div class="Text $b $b $b"> <p class="$b $b"><em>nacho</em> libre</p> <p>hello world</p> </div> <h1 class="$b"><em class="$b">hello h1</em></h1> <h4 class="$b $b"><em class="$b $b">hello h4</em></h4> <div class="Text"> <p>hello world</p> <p>nacho libre</p> </div>'));
+      expect(contents('scoped-pseudo-elements', 'scss')).to.match(hashRegex('h1.$b em.$b, h4.$b em.$b:before { color: green; } h4.$b em.$b::before { color: green; } .Text.$b:first-child > p.$b:first-child :first-child { background-color: #ff3300; } .Text.$b:first-child > p.$b:first-child:first-letter { background-color: #ff3300; } { // color: green; // } .Text.$b:first-of-type { background-color: #aae; }'));
     });
   });
 });
