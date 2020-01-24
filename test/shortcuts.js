e10260aca32a3119b9301d4b5645a96817552492
Joe Osburn <joe@jnodev.com>
2020-01-24 13:57:19 -0600

    Add customTag shortcut support

---
+++ test/shortcuts.js
@@ -0,0 +1,108 @@
+const { expect } = require('chai');
+const normalize = require('path').normalize;
+const fs = require('fs');
+
+const beard = require('../lib/index');
+
+describe('Shortcuts', function() {
+  it('allows shortcuts to be set for custom tags', function() {
+    const engine = beard({
+      templates: {
+        '/views/content': `{{@logo}}`,
+      },
+      customTags: {
+        asset: {
+          render: (path) => `/dist${path}`,
+          firstArgIsResolvedPath: true,
+          content: false
+        }
+      },
+      shortcuts: {
+        logo: {
+          tag: 'asset',
+          path: '/images/logo.png'
+        }
+      }
+    });
+    expect(engine.render('/views/content')).to.equal('/dist/images/logo.png');
+  });
+
+  it('allows shortcuts with data', function() {
+    const engine = beard({
+      templates: {
+        '/views/content': `{{@header {title: 'Welcome To The'}}}`,
+        '/header': '{{title}} component'
+      },
+      customTags: {
+        component: {
+          render: (path, data) => engine.render(path, data),
+          firstArgIsResolvedPath: true,
+          content: true
+        }
+      },
+      shortcuts: {
+        header: {
+          tag: 'component',
+          path: '/header'
+        }
+      }
+    });
+    expect(engine.render('/views/content')).to.equal('Welcome To The component');
+  });
+
+  it('allows shortcuts with block content', function() {
+    const engine = beard({
+      templates: {
+        '/templates/view': `
+          top
+          {{@header:content {title: 'title'}}}
+            some content
+          {{endheader}}`,
+        '/header': '{{title}} {{content}}'
+      },
+      customTags: {
+        component: {
+          render: (path, data) => engine.render(path, data),
+          firstArgIsResolvedPath: true,
+          content: true
+        }
+      },
+      shortcuts: {
+        header: {
+          tag: 'component',
+          path: '/header'
+        }
+      }
+    });
+    expect(engine.render('/templates/view').replace(/\s+/g, ' ')).
+      to.equal(' top title some content ');
+  });
+
+  it('allows shortcuts with periods in the name', function() {
+    const engine = beard({
+      templates: {
+        '/view': `
+          top
+          {{@page.info:content {title: 'page'}}}
+            info
+          {{endpage.info}}`,
+        '/components/info': '{{title}} {{content}}'
+      },
+      customTags: {
+        component: {
+          render: (path, data) => engine.render(path, data),
+          firstArgIsResolvedPath: true,
+          content: true
+        }
+      },
+      shortcuts: {
+        'page.info': {
+          tag: 'component',
+          path: '/components/info'
+        }
+      }
+    });
+    expect(engine.render('/view').replace(/\s+/g, ' ')).
+      to.equal(' top page info ');
+  });
+});
