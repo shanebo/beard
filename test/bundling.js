4d3c9eb3e7a6d1f0b64ff993195df16674bb52d3
Joe Osburn <joe@jnodev.com>
2020-10-13 16:16:34 -0500

    Convert lang test from less to pcss

--- test/bundling.js
+++ test/bundling.js
@@ -69,7 +69,7 @@ describe('Bundling', function() {
 
   describe('when style block has lang attribute', function() {
     it('sets file extension on extracted block file', function() {
-      expect(contents('lang', 'less')).to.equalIgnoreSpaces(`@color: blue; body { color: @color; }`);
+      expect(contents('lang', 'pcss')).to.equalIgnoreSpaces(`body { color: blue; }`);
     });
   });
 
